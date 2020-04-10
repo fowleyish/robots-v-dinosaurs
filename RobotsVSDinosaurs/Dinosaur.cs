f6b17f075907d95c4e5faa87358ba6bb77c777fb
Fowley <alexanderfowle@gmail.com>
2020-04-10 13:51:19 -0500

    Init - building class containers

---
+++ RobotsVSDinosaurs/Dinosaur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotsVSDinosaurs
+{
+    class Dinosaur
+    {
+
+        public string name = "";
+        public int hp = 0;
+
+
+    }
+}
